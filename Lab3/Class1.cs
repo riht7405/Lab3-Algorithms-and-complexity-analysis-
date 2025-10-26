bfbc547cc794e212f348ce8ad53839f2781432e3
riht7405 <fly200fly@gmail.com.com>
2025-10-26 15:29:05 +0500

    Class стека

---
+++ Lab3/Class1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+// Узел связного списка
+public class Node<T>
+{
+    public T Data { get; set; }
+    public Node<T> Next { get; set; }
+
+    public Node(T data)
+    {
+        Data = data;
+        Next = null;
+    }
+}
+
+// Связный список
+public class LinkedList<T>
+{
+    private Node<T> head;
+
+    public void PushFront(T data)
+    {
+        Node<T> newNode = new Node<T>(data);
+        newNode.Next = head;
+        head = newNode;
+    }
+
+    public T PopFront()
+    {
+        if (head == null)
+            throw new InvalidOperationException("List is empty");
+
+        T data = head.Data;
+        head = head.Next;
+        return data;
+    }
+
+    public T Front()
+    {
+        if (head == null)
+            throw new InvalidOperationException("List is empty");
+
+        return head.Data;
+    }
+
+    public bool IsEmpty()
+    {
+        return head == null;
+    }
+
+    public void Print()
+    {
+        Node<T> current = head;
+        List<string> elements = new List<string>();
+
+        while (current != null)
+        {
+            elements.Add(current.Data.ToString());
+            current = current.Next;
+        }
+
+        Console.WriteLine(string.Join(" ", elements));
+    }
+}
+
+// Стек на основе связного списка
+public class Stack<T>
+{
+    private LinkedList<T> list;
+
+    public Stack()
+    {
+        list = new LinkedList<T>();
+    }
+
+    public void Push(T elem)
+    {
+        list.PushFront(elem);
+    }
+
+    public T Pop()
+    {
+        return list.PopFront();
+    }
+
+    public T Top()
+    {
+        return list.Front();
+    }
+
+    public bool IsEmpty()
+    {
+        return list.IsEmpty();
+    }
+
+    public void Print()
+    {
+        list.Print();
+    }
+}
